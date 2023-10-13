c2a77076360b8b6fc66297936244043ab32c889e
lucaslagant <lucaslagant@gmail.com>
2023-10-13 10:14:01 +0200

    pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
+import PokemonCollection from './components/PokemonCollection';
+import { Pokemon } from './interface';
+
+interface Pokemons {
+  name: string
+  url: string
+}
+
+
 
 function App() {
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [nextUrl, setNextUrl] = useState<string>("")
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+        )
+
+        setNextUrl(res.data.next)
+        res.data.results.forEach(async (pokemon: Pokemons) => {
+          const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+
+          setPokemons((p) => [...p, poke.data])
+        })
+    }
+    getPokemon()
+  }, [])
+
+  const nextPage = async () => {
+    let res = await axios.get(nextUrl)
+
+    setNextUrl(res.data.next)
+
+    setNextUrl(res.data.next)
+        res.data.results.forEach(async (pokemon: Pokemons) => {
+          const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+
+          setPokemons((p) => [...p, poke.data])
+        })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="pokemon-header">Pokemon</header>
+      <PokemonCollection pokemons={pokemons} />
+      <button onClick={nextPage}>Charger</button>
     </div>
   );
 }
---
+++ src/components/PokemonCollection.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Pokemon } from '../interface'
+import "./pokemon.css"
+import PokemonList from './PokemonList'
+
+interface Props {
+  pokemons: Pokemon[]
+}
+
+const PokemonCollection: React.FC<Props> = (props) => {
+  const { pokemons } = props
+
+
+  return <section className='collection-container'>
+    {pokemons.map((pokemon) => {
+      return (
+      <PokemonList
+        key={pokemon.id}
+        name={pokemon.name}
+        id={pokemon.id}
+        image= {pokemon.sprites.front_default}
+        type={pokemon.types[0].type.name} />
+      )
+    })}
+  </section>
+}
+
+export default PokemonCollection
